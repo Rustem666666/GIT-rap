0321cd7d1cd2915e0cbd74a10d9b6e9b47417731
Rustem666666 <rustem666666@gmail.com>
2019-03-21 23:45:03 +0300

    Задание 4

---
+++ 4/Program 4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Обмен значений с использованием третьей переменной
+            int a, b, c;
+            Console.WriteLine("Обмен значений с использованием третьей переменной.");
+            Console.WriteLine("Введите значение a: ");
+            a = Convert.ToInt32(Console.ReadLine());     // Присваиваем начальное значение a
+            Console.WriteLine("Введите значение b: ");
+            b = Convert.ToInt32(Console.ReadLine());     // Присваиваем начальное значение b
+            c = a;      // В c запоминаем значение a
+            a = b;      // В a записываем b
+            b = c;      // В b записываем сохраненное a
+            Console.WriteLine($"Новое значение a={a},b={b}.");
+            Console.ReadLine();
+
+            //Обмен значений без использования третьей переменной
+            Console.WriteLine("Обмен значений без использования третьей переменной.");
+            Console.WriteLine("Введите значение a: ");
+            a = Convert.ToInt32(Console.ReadLine());     // Присваиваем начальное значение a
+            Console.WriteLine("Введите значение b: ");
+            b = Convert.ToInt32(Console.ReadLine());     // Присваиваем начальное значение b
+            a = a + b;  // Присваиваем a сумму значений a и b
+            b = a - b;  // Присваиваем b разность между новым значением a и переменной b
+            a = a - b;  // Присваиваем a результат вычитания b из обновленного значения a. Готово
+            Console.WriteLine($"Новое значение a={a},b={b}.");
+            Console.ReadLine();
+        }
+    }
+}
